5a251b790588b7a08e667341d01f907a947779cb
vrn21 <killmongernvn@gmail.com>
2023-04-17 23:48:00 +0530

    Initial commit

--- donut.c
+++ donut.c
@@ -7,12 +7,12 @@ double sin(),cos();
 int main(){
  float A = 0, B = 0, i, j, z[1760];
  char b[1760];
- printf("\n1b[2J");
+ printf("\x1b[2J");
  for(;;){
   memset(b, 32, 1760);
   memset(z, 0, 7040);
-  for ( j = 0;6.28>j; j+= 0.01){
-   for (i = 0; 6.28 > i; i += 0.08){
+  for ( j = 0;6.28>j; j+= 0.008){
+   for (i = 0; 6.28 > i; i += 0.07){
     float c = sin(i), d = cos(j), e = sin(A), f = sin(j), g = cos(A), h = d + 2, D = 1/ (c*h*e + f*g + 5), l = cos(i),
           m = cos(B), n = sin(B), t = c*h*g - f * e;
 
